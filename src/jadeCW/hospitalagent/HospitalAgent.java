09f357075739b2d2913f2109ebdb633bde64cd48
Chris <cb2109@imperial.ic.ac.uk>
2014-02-06 16:07:33 +0000

    Added the class casting from object to integer in the hospital agent.

--- src/jadeCW/hospitalagent/HospitalAgent.java
+++ src/jadeCW/hospitalagent/HospitalAgent.java
@@ -17,7 +17,14 @@ public class HospitalAgent extends Agent {
         Object[] input = getArguments();
 
         if(input.length <= 0) {
-            throw new ArrayIndexOutOfBoundsException("The Input array did not contain ");
+            throw new ArrayIndexOutOfBoundsException("The Input array did not contain any elements.");
+        }
+
+        try {
+            Integer i = (Integer) input[0];
+        } catch(ClassCastException e) {
+            throw new ClassCastException("The input array did not contain the total number of " +
+                    "available appointments as the first argument.");
         }
 
     }
